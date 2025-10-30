7b8e3726b61b65fbfc9a3421d62cffff119b8b06
srsxnsh <srsxnsh@proton.me>
2025-10-30 21:14:32 +0000

    need to add a sound...

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,1 +1,55 @@
 #!/usr/bin/env bash
+
+function get_pos_int {
+    message=$1
+
+    read -p "$message" userinput
+    while [[ ! "$userinput" =~ ^[0-9]+$ ]]; do
+        read -p "Please input a positive integer: " userinput
+    done
+
+    echo $userinput
+
+}
+
+function do_timer {
+    duration=$1
+
+    for ((t=$duration; t>-1; t--)); do
+        printf "\rTime left: %02d:%02d" $((t/60)) $((t%60))
+        sleep 1
+    done
+
+}
+
+function notif {
+    message=$1
+    sound=$2
+    printf "$message"
+    notify-send "$message"
+    paplay "sound"
+
+}
+
+
+PING_SOUND=~/ping.wav
+if [ ! -f "$PING_SOUND" ]; then
+    curl -L -o "$PING_SOUND" #uhhhh get a sound ig lmao
+fi
+
+
+
+workmins=$(get_pos_int 'Enter work time:')
+breakmins=$(get_pos_int 'Enter break time:')
+
+workseconds=$((workmins * 60))
+breakseconds=$((breakmins * 60))
+
+while true; do
+    do_timer workseconds
+    notif 'Break time!' PING_SOUND
+    do_timer breakseconds
+    notif 'Work time!' PING_SOUND
+done
+
+
